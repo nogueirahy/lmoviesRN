9608e09be8e9ce4f15cc939e501e75ae9bb18db4
Leonardo Nogueira <nogueirahy@gmail.com>
2019-07-20 22:41:42 -0300

    Added Test configureStore

---
+++ src/store/__tests__/configureStore.spec.js
@@ -0,0 +1,29 @@
+import { createStore, compose, applyMiddleware } from "redux";
+
+require("../configureStore");
+
+let createSagaMiddleware;
+
+jest.mock("redux", () => ({
+  createStore: jest.fn(),
+  compose: jest.fn(),
+  applyMiddleware: jest.fn()
+}));
+
+jest.mock(
+  "redux-saga",
+  () => (createSagaMiddleware = jest.fn().mockReturnValue({ run: jest.fn() }))
+);
+
+jest.mock("../reducers", () => ({
+  persistedReducer: jest.fn()
+}));
+
+describe("configureStore", () => {
+  it("deve confiturar a store", () => {
+    expect(createSagaMiddleware).toBeCalled();
+    expect(applyMiddleware).toBeCalled();
+    expect(createStore).toBeCalled();
+    expect(compose).toBeCalled();
+  });
+});
