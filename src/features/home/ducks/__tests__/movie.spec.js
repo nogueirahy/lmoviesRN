d3bc78451ccafd128ad16b1ca1da270b46dac9ec
Leonardo Nogueira <nogueirahy@gmail.com>
2019-07-20 22:23:54 -0300

    Added Test reducer

---
+++ src/features/home/ducks/__tests__/movie.spec.js
@@ -0,0 +1,33 @@
+import { INITIAL_STATE, reducer, Creators } from "../movie";
+
+describe("ducks home", () => {
+  describe("Quando executar as actions", () => {
+    it("Deve alterar o estado para movieRequest", () => {
+      const page = 1;
+      const action = Creators.movieRequest(page);
+      expect(reducer(undefined, action)).toEqual({
+        ...INITIAL_STATE,
+        page,
+        isFetching: true
+      });
+    });
+
+    it("Deve alterar o estado para movieSuccess", () => {
+      const data = [];
+      const action = Creators.movieSuccess(data);
+      expect(reducer(undefined, action)).toEqual({
+        ...INITIAL_STATE,
+        data,
+        isFetching: false
+      });
+    });
+
+    it("Deve alterar o estado para movieFailure", () => {
+      const action = Creators.movieFailure();
+      expect(reducer(undefined, action)).toEqual({
+        ...INITIAL_STATE,
+        isFetching: false
+      });
+    });
+  });
+});
