09fb34b50dec1a8dba25b7141e4202c6a850a442
Leonardo Nogueira <leonardo.nogueira@dextra-sw.com>
2021-03-27 22:08:49 -0300

    added interface

---
+++ src/features/home/interfaces.ts
@@ -0,0 +1,12 @@
+import { ApiOkResponse } from "apisauce";
+
+interface IResponseData {
+  results: object;
+  total_pages: string;
+}
+
+export type TResponse = ApiOkResponse<IResponseData>;
+
+export interface IParams {
+  page: number;
+}
